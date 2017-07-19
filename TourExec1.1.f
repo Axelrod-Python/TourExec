256958495fae319ce8d7b97318c9f47612ccac11
Owen Campbell <owen.campbell@tanti.org.uk>
2017-07-19 13:45:30 +0100

    Fix line length and equality check problems

--- TourExec1.1.f
+++ TourExec1.1.f
@@ -27,7 +27,8 @@ c Next few lines  are control parameters
       integer MoveRecord(308)             ! Moves of current game
       character*9 day
       character*8 timenow
-      integer ActualTFTTourSc(63)/453,453,453,453,453,  453,453,452,453,453,
+      integer ActualTFTTourSc(63)/
+     1 453,453,453,453,453,  453,453,452,453,453,
      1 453,453,453,453,453,   449,453,452,450,453,
      2 453,453,453,453,452,   453,446,453,449,453,
      3 453,453,453,453,453,   453,453,453,452,453,
@@ -40,7 +41,8 @@ c Next few lines  are control parameters
       call Date(day)
       call TIME(timenow)
       write(6,100) Version, day, timenow
-100   format('  Ax TourExec Program Output, Version ',f6.2, '.', 1H, A10, A10)
+100   format('  Ax TourExec Program Output, Version ',f6.2, '.', 1H,
+     1 A10, A10)
       RandomSeed = Jsecnds(0)                     ! uses elapsed time since midnight as random seed
 c   RandomSeed=66222                                ! Uses fixed random number
       Write(6,103) RandomSeed
@@ -50,10 +52,12 @@ c   RandomSeed=66222                                ! Uses fixed random number
 85    format(' Noise (per choice) = ', f8.4)
 
       write(6, 104) ColType
-104   format(' Col Type, 1=TFT, 2=TF2F, 3=Random, 4=Pavlov. Col Type = ', i3)
-      if (movereport=1)  write(6, 105)
-105   format(' Move report: 1 means R, 2 means T, 3  means S, 4 means P for column.')
-      if (GameReport=1) write(6,101)
+104   format(' Col Type, 1=TFT, 2=TF2F, 3=Random, 4=Pavlov. Col Type = '
+     1 , i3)
+      if (movereport==1) write(6, 105)
+105   format(' Move report: 1 means R, 2 means T, 3  means S, 4 means P
+     1 for column.')
+      if (GameReport==1) write(6,101)
 101   format(' Rank  Game RScore CScore #ColR #ColT #ColS #ColP')
       ITotalColPoints = 0                                 ! Initialize Col's total points
       Do 30 row= minRow,maxRow                    ! normally 1 to 63
@@ -71,10 +75,12 @@ c   RandomSeed=66222                                ! Uses fixed random number
 10    Continue            ! End Do tallyType
             Do 15 Move = 1, Length(Game)
                 RandomNumber = RAN(RandomSeed)
-                RowChoice = KRowFunction(JB,Move, RowGameSc,ColGameSc,RandomNumber,Row,JA)
+                RowChoice = KRowFunction(JB,Move, RowGameSc,ColGameSc,
+     1            RandomNumber,Row,JA)
                 if ( RAN(RandomSeed) < noise ) RowChoice = 1-RowChoice  !  noise happened to Row
                 RandomNumber = RAN(RandomSeed)
-                ColChoice = KColFunction(JA,Move,ColGameSc,RowGameSc,RandomNumber,ColType,JB)
+                ColChoice = KColFunction(JA,Move,ColGameSc,RowGameSc,
+     1             RandomNumber,ColType,JB)
                 if ( RAN(RandomSeed) < noise ) ColChoice = 1 - ColChoice ! noise happened to Col
 C temp test:
 c               Write(6, 999) Move, RowChoice, ColChoice
@@ -103,18 +109,19 @@ c999    Format(' move, rowchoice, colchoice ', 3i6)
 C  write game output
             RowPairSc=RowPairSc+RowGameSc        ! sum over 5 games
             ColPairSc=ColPairSc+ColGameSc
-            if (GameReport=1) Write(6, 110)  Rank, Game, RowGameSc,
+            if (GameReport==1) Write(6, 110)  Rank, Game, RowGameSc,
      1 ColGameSc, Tally(1), Tally(2), Tally(3), Tally(4)
 110   format(9i6, 10i3)
-      if (movereport .eq. 1) write(6, 112) (MoveRecord(ir), ir=1,length(game))
+      if (movereport .eq. 1) write(6, 112) (MoveRecord(ir), ir=1,
+     1   length(game))
 112   format('   ', 10i2, 2H, 10i2, 2H, 10i2, 2H, 10i2)
 20    Continue        ! End Do Game
-      if (GameReport=1) write(6, 115) RowPairSc, ColPairSc
+      if (GameReport==1) write(6, 115) RowPairSc, ColPairSc
       IRowPairSc(Row) = RowPairSc             !  total over 5 games
       IColPairSc(Row) = ColPairSc
       IColTourSc = IColTourSc +ColPairSc  ! running total of col's points
-115   format(' Totals over 5 games: RowPairSc= ',I7, ' ColPairSc = ', I7)
-      if (GameReport=1) write (6, 120)
+115   format('Totals over 5 games: RowPairSc= ',I7, ' ColPairSc = ', I7)
+      if (GameReport==1) write (6, 120)
 120   format()
 
 30    Continue        ! End Do Row
@@ -122,19 +129,22 @@ C  write game output
 C final report: calc tour score, write tour output
 
       Write(6, 135)
-135   format(' Rank    RowSc   ColSc   AveRowSc AveColSc 2ndRndTFT  2ndRndTFT-Col')
+135   format(' Rank    RowSc   ColSc   AveRowSc AveColSc 2ndRndTFT
+     1 2ndRndTFT-Col')
       Do 40 Row = minRow,maxRow
       IRowTourPairSc = IRowPairSc(Row)/5
       IColTourPairSc = IColPairSc(Row)/5
       ITotalColPoints =  ITotalColPoints + IColPairSc(Row)    ! accumulate col points
-      Write(6, 140) Row, IRowPairSc(Row), IColPairSc(Row),IRowTourPairSc,
-     2 IColTourPairSc, ActualTFTTourSc(Row), ActualTFTTourSc(Row)-IColTourPairSc
+      Write(6, 140) Row, IRowPairSc(Row), IColPairSc(Row),
+     1 IRowTourPairSc,IColTourPairSc, ActualTFTTourSc(Row),
+     2 ActualTFTTourSc(Row)-IColTourPairSc
 140   format(i6, 4i8, '    ',i8,'    ',i8)
 40    continue        ! end final report
       TotalColPoints = ITotalColPoints                ! to make floating point (total over 63*5 games)
       ColTourSc =(TotalColPoints/5 )/63   !   Ave per game over 63 pairs
       write(6, 150) ColType,  ITotalColPoints, ColTourSc
-150   format(' Col Type= ', i4, '. Col Pts = ', i7, '   Col"s Tour Sc = ', f7.3)
+150   format(' Col Type= ', i4, '. Col Pts = ', i7, '   Cols Tour Sc = '
+     1 , f7.3)
       end   ! Main Program
 C-----------------------------
       Function KColFunction(J,M,K,L,R,IColType,JB)        ! Look up col rule, return col choice
@@ -183,95 +193,95 @@ c add JA to row fcns to report their own previous move, 7/23/93
       if (irow>16 ) goto 117
       if (irow>8 ) goto 109
       if (irow>4 ) goto 105
-      if(irow=1) KRowFunction = K92R(J,M,K,L,R,JA)
-      if(irow=2) KRowFunction = K61R(J,M,K,L,R,JA)
-      if(irow=3) KRowFunction = K42R(J,M,K,L,R,JA)
-      if(irow=4) KRowFunction = K49R(J,M,K,L,R,JA)
+      if(irow==1) KRowFunction = K92R(J,M,K,L,R,JA)
+      if(irow==2) KRowFunction = K61R(J,M,K,L,R,JA)
+      if(irow==3) KRowFunction = K42R(J,M,K,L,R,JA)
+      if(irow==4) KRowFunction = K49R(J,M,K,L,R,JA)
       return
-105   if(irow=5) KRowFunction = K44R(J,M,K,L,R,JA)
-      if(irow=6) KRowFunction = K60R(J,M,K,L,R,JA)
-      if(irow=7) KRowFunction = K41R(J,M,K,L,R,JA)
-      if(irow=8) KRowFunction = K75R(J,M,K,L,R,JA)
+105   if(irow==5) KRowFunction = K44R(J,M,K,L,R,JA)
+      if(irow==6) KRowFunction = K60R(J,M,K,L,R,JA)
+      if(irow==7) KRowFunction = K41R(J,M,K,L,R,JA)
+      if(irow==8) KRowFunction = K75R(J,M,K,L,R,JA)
       return
 109   if(irow>12) goto 113
-      if(irow=9) KRowFunction = K84R(J,M,K,L,R,JA)
-      if(irow=10) KRowFunction = K32R(J,M,K,L,R,JA)
-      if(irow=11) KRowFunction = K35R(J,M,K,L,R,JA)
-      if(irow=12) KRowFunction = K68R(J,M,K,L,R,JA)
+      if(irow==9) KRowFunction = K84R(J,M,K,L,R,JA)
+      if(irow==10) KRowFunction = K32R(J,M,K,L,R,JA)
+      if(irow==11) KRowFunction = K35R(J,M,K,L,R,JA)
+      if(irow==12) KRowFunction = K68R(J,M,K,L,R,JA)
       return
-113   if(irow=13) KRowFunction = K72R(J,M,K,L,R,JA)
-      if(irow=14) KRowFunction = K46R(J,M,K,L,R,JA)
-      if(irow=15) KRowFunction = K83R(J,M,K,L,R,JA)
-      if(irow=16) KRowFunction = K47R(J,M,K,L,R,JA)
+113   if(irow==13) KRowFunction = K72R(J,M,K,L,R,JA)
+      if(irow==14) KRowFunction = K46R(J,M,K,L,R,JA)
+      if(irow==15) KRowFunction = K83R(J,M,K,L,R,JA)
+      if(irow==16) KRowFunction = K47R(J,M,K,L,R,JA)
       return
 117   if (irow>24 ) goto 125
       if (irow>20 ) goto 121
-      if(irow=17) KRowFunction = K64R(J,M,K,L,R,JA)
-      if(irow=18) KRowFunction = K51R(J,M,K,L,R,JA)
-      if(irow=19) KRowFunction = K78R(J,M,K,L,R,JA)
-      if(irow=20) KRowFunction = K66R(J,M,K,L,R,JA)
+      if(irow==17) KRowFunction = K64R(J,M,K,L,R,JA)
+      if(irow==18) KRowFunction = K51R(J,M,K,L,R,JA)
+      if(irow==19) KRowFunction = K78R(J,M,K,L,R,JA)
+      if(irow==20) KRowFunction = K66R(J,M,K,L,R,JA)
       return
-121   if(irow=21) KRowFunction = K58R(J,M,K,L,R,JA)
-      if(irow=22) KRowFunction = K88R(J,M,K,L,R,JA)
-      if(irow=23) KRowFunction = K31R(J,M,K,L,R,JA)
-      if(irow=24) KRowFunction = K90R(J,M,K,L,R,JA)
+121   if(irow==21) KRowFunction = K58R(J,M,K,L,R,JA)
+      if(irow==22) KRowFunction = K88R(J,M,K,L,R,JA)
+      if(irow==23) KRowFunction = K31R(J,M,K,L,R,JA)
+      if(irow==24) KRowFunction = K90R(J,M,K,L,R,JA)
       return
 125   if (irow>28 ) goto 129
-      if(irow=25) KRowFunction = K39R(J,M,K,L,R,JA)
-      if(irow=26) KRowFunction = K79R(J,M,K,L,R,JA)
-      if(irow=27) KRowFunction = K67R(J,M,K,L,R,JA)
-      if(irow=28) KRowFunction = K86R(J,M,K,L,R,JA)
+      if(irow==25) KRowFunction = K39R(J,M,K,L,R,JA)
+      if(irow==26) KRowFunction = K79R(J,M,K,L,R,JA)
+      if(irow==27) KRowFunction = K67R(J,M,K,L,R,JA)
+      if(irow==28) KRowFunction = K86R(J,M,K,L,R,JA)
       return
-129   if(irow=29) KRowFunction = K69R(J,M,K,L,R,JA)
-      if(irow=30) KRowFunction = K91R(J,M,K,L,R,JA)
-      if(irow=31) KRowFunction = K57R(J,M,K,L,R,JA)
-      if(irow=32) KRowFunction = K70R(J,M,K,L,R,JA)
+129   if(irow==29) KRowFunction = K69R(J,M,K,L,R,JA)
+      if(irow==30) KRowFunction = K91R(J,M,K,L,R,JA)
+      if(irow==31) KRowFunction = K57R(J,M,K,L,R,JA)
+      if(irow==32) KRowFunction = K70R(J,M,K,L,R,JA)
       return
 133   if (irow>48 ) goto 149
       if (irow>40 ) goto 141
       if (irow>36 ) goto 137
-      if(irow=33) KRowFunction = K85R(J,M,K,L,R,JA)
-      if(irow=34) KRowFunction = K38R(J,M,K,L,R,JA)
-      if(irow=35) KRowFunction = K40R(J,M,K,L,R,JA)
-      if(irow=36) KRowFunction = K80R(J,M,K,L,R,JA)
+      if(irow==33) KRowFunction = K85R(J,M,K,L,R,JA)
+      if(irow==34) KRowFunction = K38R(J,M,K,L,R,JA)
+      if(irow==35) KRowFunction = K40R(J,M,K,L,R,JA)
+      if(irow==36) KRowFunction = K80R(J,M,K,L,R,JA)
       return
-137   if(irow=37) KRowFunction = K37R(J,M,K,L,R,JA)
-      if(irow=38) KRowFunction = K56R(J,M,K,L,R,JA)
-      if(irow=39) KRowFunction = K43R(J,M,K,L,R,JA)
-      if(irow=40) KRowFunction = K59R(J,M,K,L,R,JA)
+137   if(irow==37) KRowFunction = K37R(J,M,K,L,R,JA)
+      if(irow==38) KRowFunction = K56R(J,M,K,L,R,JA)
+      if(irow==39) KRowFunction = K43R(J,M,K,L,R,JA)
+      if(irow==40) KRowFunction = K59R(J,M,K,L,R,JA)
       return
 141   if(irow>44) goto 145
-      if(irow=41) KRowFunction = K73R(J,M,K,L,R,JA)
-      if(irow=42) KRowFunction = K55R(J,M,K,L,R,JA)
-      if(irow=43) KRowFunction = K81R(J,M,K,L,R,JA)
-      if(irow=44) KRowFunction = K87R(J,M,K,L,R,JA)
+      if(irow==41) KRowFunction = K73R(J,M,K,L,R,JA)
+      if(irow==42) KRowFunction = K55R(J,M,K,L,R,JA)
+      if(irow==43) KRowFunction = K81R(J,M,K,L,R,JA)
+      if(irow==44) KRowFunction = K87R(J,M,K,L,R,JA)
       return
-145   if(irow=45) KRowFunction = K53R(J,M,K,L,R,JA)
-      if(irow=46) KRowFunction = K76R(J,M,K,L,R,JA)
-      if(irow=47) KRowFunction = K65R(J,M,K,L,R,JA)
-      if(irow=48) KRowFunction = K52R(J,M,K,L,R,JA)
+145   if(irow==45) KRowFunction = K53R(J,M,K,L,R,JA)
+      if(irow==46) KRowFunction = K76R(J,M,K,L,R,JA)
+      if(irow==47) KRowFunction = K65R(J,M,K,L,R,JA)
+      if(irow==48) KRowFunction = K52R(J,M,K,L,R,JA)
       return
 149   if (irow>56 ) goto 157
       if (irow>52 ) goto 153
-      if(irow=49) KRowFunction = K82R(J,M,K,L,R,JA)
-      if(irow=50) KRowFunction = K45R(J,M,K,L,R,JA)
-      if(irow=51) KRowFunction = K62R(J,M,K,L,R,JA)
-      if(irow=52) KRowFunction = K34R(J,M,K,L,R,JA)
+      if(irow==49) KRowFunction = K82R(J,M,K,L,R,JA)
+      if(irow==50) KRowFunction = K45R(J,M,K,L,R,JA)
+      if(irow==51) KRowFunction = K62R(J,M,K,L,R,JA)
+      if(irow==52) KRowFunction = K34R(J,M,K,L,R,JA)
       return
-153   if(irow=53) KRowFunction = K48R(J,M,K,L,R,JA)
-      if(irow=54) KRowFunction = K50R(J,M,K,L,R,JA)
-      if(irow=55) KRowFunction = K77R(J,M,K,L,R,JA)
-      if(irow=56) KRowFunction = K89R(J,M,K,L,R,JA)
+153   if(irow==53) KRowFunction = K48R(J,M,K,L,R,JA)
+      if(irow==54) KRowFunction = K50R(J,M,K,L,R,JA)
+      if(irow==55) KRowFunction = K77R(J,M,K,L,R,JA)
+      if(irow==56) KRowFunction = K89R(J,M,K,L,R,JA)
       return
 157   if (irow>60) goto 161
-      if(irow=57) KRowFunction = K63R(J,M,K,L,R,JA)
-      if(irow=58) KRowFunction = K54R(J,M,K,L,R,JA)
-      if(irow=59) KRowFunction = K33R(J,M,K,L,R,JA)
-      if(irow=60) KRowFunction = K71R(J,M,K,L,R,JA)
+      if(irow==57) KRowFunction = K63R(J,M,K,L,R,JA)
+      if(irow==58) KRowFunction = K54R(J,M,K,L,R,JA)
+      if(irow==59) KRowFunction = K33R(J,M,K,L,R,JA)
+      if(irow==60) KRowFunction = K71R(J,M,K,L,R,JA)
       return
-161   if(irow=61) KRowFunction = K74R(J,M,K,L,R,JA)
-      if(irow=62) KRowFunction = K93R(J,M,K,L,R,JA)
-      if(irow=63) KRowFunction = K36R(J,M,K,L,R,JA)
+161   if(irow==61) KRowFunction = K74R(J,M,K,L,R,JA)
+      if(irow==62) KRowFunction = K93R(J,M,K,L,R,JA)
+      if(irow==63) KRowFunction = K36R(J,M,K,L,R,JA)
       return
       END
 c----------------------------------------------------
